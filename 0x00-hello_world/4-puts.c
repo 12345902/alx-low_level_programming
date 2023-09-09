5c26192add10faa07989b554fb98d9e051233ff2
12345902 <ji.mbadiwe@stu.unizik.edu.ng>
2023-09-09 15:22:07 -0700

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
+
 /**
- * main - this is the main function
- *
- * Return:always 0
+ * main - display  "whatever is written in the print bracket"
+ * Return: 0 and exit the program
  */
-
 int main(void)
-
 {
-
-	puts("\"Programming is like building a multilingual puzzle") :
-	return(0) :
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
