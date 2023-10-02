534fa2c7203e33a88d71b1b7a54200814fa6aa1c
12345902 <ji.mbadiwe@stu.unizik.edu.ng>
2023-10-01 17:06:24 -0700

    puts-recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,10 +1,11 @@
 #include "main.h"
+
 /**
- * _puts_recursion -prints a string followed by a new line.
- *  @s:type int string
- *  Return :Always 0
+ * _puts_recursion - Prints a string followed by a new line.
+ * @s: type int string
+ * Return: Always 0.
  */
-void _puts_recursion(char *s);
+void _puts_recursion(char *s)
 {
 	int x = 0;
 
