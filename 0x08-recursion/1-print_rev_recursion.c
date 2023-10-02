3c46cfc610e0cdbbf210e3912f108c5dd5f9c2ae
12345902 <ji.mbadiwe@stu.unizik.edu.ng>
2023-10-01 18:26:16 -0700

    print_rev- recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,16 +1,18 @@
 #include "main.h"
+
 /**
- * print_rev_recursion - prints a string in reverse.
- * @s:the strings
+ * _print_rev_recursion - prints a string in reverse
+ * @s: the string
  */
+
 void _print_rev_recursion(char *s)
 {
 	int i = 0;
+
 	if (s[i] == '\0')
 	{
 		return;
 	}
-	_print-rev-recursion(s + 1);
+	_print_rev_recursion(s + 1);
 	_putchar(s[i]);
-
 }
