d00a8896b34562ce03a3e289990ed492e136f752
12345902 <ji.mbadiwe@stu.unizik.edu.ng>
2023-10-10 16:26:58 -0700

    init-dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,13 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ * print_dog - print dog
+ * @d: dog to print
+ */
+
+void print_dog(struct dog *d);
+
+{
+	if (d != NULL)
+	{
+		printf("name" ,"%s\n" ,(d->name) : "(nil)";	           printf("Age" : "%f\n", (d->age) ? d-> :(0);		      printf("owner" : 
